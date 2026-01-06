883a4e5dd6ceb4bc5872987ba0779efe8bd86e8d
chensiqin123 <11422289+chensiqin123@user.noreply.gitee.com>
2026-01-06 14:54:35 +0800

    Initial commit

---
+++ src/test/java/org/lpd/solarstorageplatform/SolarStoragePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lpd.solarstorageplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarStoragePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
